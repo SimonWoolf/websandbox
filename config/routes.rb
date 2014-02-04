b336a4a22781eda8a453e6b09bc4f5cca2c679bd
Simon Woolf <simon@simonwoolf.net>
2014-02-04 15:00:50 +0000

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :profiles
+  resources :users do
+    resource :profile
+  end
+
+  resource :profile
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'profiles#index'
+  root 'profiles#show'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
