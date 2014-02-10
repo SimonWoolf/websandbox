f4403afade502aade704520b01e58d84f03da2a5
Simon Woolf <simon@simonwoolf.net>
2014-02-10 13:18:24 +0000

    js style fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,19 +16,19 @@
 //= require_tree .
 
 function updateHTML(text, clickedElement){
-  $elem = $(text)
+  var $elem = $(text);
   if($elem.length > 1) {
-    show_flash("One tag only", "warning")
+    show_flash("One tag only", "warning");
   } else {
-    $(clickedElement).replaceWith($elem)
-    $elem.draggable()
+    $(clickedElement).replaceWith($elem);
+    $elem.draggable();
     return $elem;
   }
 }
 
 function updateFromFieldsAndHidePanel(clickedElement){
-  var text = $('#edit_field_html').val()
-  var css = $('#edit_field_css').val()
+  var text = $('#edit_field_html').val();
+  var css = $('#edit_field_css').val();
   clickedElement = updateHTML(text, clickedElement)[0];
   $(clickedElement).attr('style', css);
   $('#edit_panel').hide();
@@ -37,40 +37,40 @@ function updateFromFieldsAndHidePanel(clickedElement){
 $(function() {
   $(document).foundation();
 
-  $('#edit_panel').draggable()
+  $('#edit_panel').draggable();
 
-  $('.profile_content *').draggable({ disabled: true })
+  $('.profile_content *').draggable({ disabled: true });
 
-  $('#edit_button').on('click', function(e){
+  $('#edit_button').on('click', function(){
     $('#grid-background').fadeToggle('slow');
-    $('.profile_content').toggleClass('uneditable')
-    $(this).toggleClass('editing')
+    $('.profile_content').toggleClass('uneditable');
+    $(this).toggleClass('editing');
     if($(this).hasClass('editing')){
-      $(this).html('Stop editing')
-      $('.profile_content *').draggable('enable')
+      $(this).html('Stop editing');
+      $('.profile_content *').draggable('enable');
     }else{
-      $(this).html('Edit')
-      $('.profile_content *').draggable('disable')
+      $(this).html('Edit');
+      $('.profile_content *').draggable('disable');
     }
-  })
+  });
 
    //Only register *click* for editable elements
   var clickedElement;
-  var uneditable = 'html,body,.uneditable,.uneditable *,#uneditable'
-  var editable = '*:not(' + uneditable + ')'
+  var uneditable = 'html,body,.uneditable,.uneditable *,#uneditable';
+  var editable = '*:not(' + uneditable + ')';
 
 	$('*').on('click', function(event){
-		if($(event.target).closest('#edit_panel').length) return true;
+		if($(event.target).closest('#edit_panel').length) {return true;}
 		if($(event.target).is(uneditable) && $('#edit_panel').is(':visible')) {
 			updateFromFieldsAndHidePanel(clickedElement);
 		}
-  })
+  });
 
   $('.profile_content').on('click', editable, function(event){
-    console.log(event.target.outerHTML)
+    console.log(event.target.outerHTML);
     $('#edit_panel').show();
     $('#edit_panel').css({'left': event.target.clientLeft + 15 + 'px',
-                          'top': event.target.clientHeight + event.target.offsetTop + 'px'})
+                          'top': event.target.clientHeight + event.target.offsetTop + 'px'});
     $('#edit_field_css').val($(event.target)
                              .attr('style')
                              .replace(/;\s?/g, ";\n") //put newlines after semicolons in css
@@ -80,28 +80,28 @@ $(function() {
                               .removeAttr("style")
                               .prop('outerHTML')
                              );
-    clickedElement = event.target
-  })
+    clickedElement = event.target;
+  });
 
-  $('.profile_content').on('mouseenter', editable, function(event){
-    $(this).css({'border':'2px dashed red'})
-  })
+  $('.profile_content').on('mouseenter', editable, function(){
+    $(this).css({'border':'2px dashed red'});
+  });
   $('.profile_content').on('mouseleave', editable, function(){
-    $(this).css({'border':'none'})
-  })
+    $(this).css({'border':'none'});
+  });
 
   $('#edit_field_html,#edit_field_css').on('keydown',function(pressed){
-    if(pressed.keyCode == 13 && pressed.shiftKey){ // shift+enter
+    if(pressed.keyCode === 13 && pressed.shiftKey){ // shift+enter
       // do not override -- so can use this to insert a newline
-    }else if(pressed.keyCode == 13){ //enter
+    }else if(pressed.keyCode === 13){ //enter
       pressed.preventDefault();
       updateFromFieldsAndHidePanel(clickedElement);
-    }else if(pressed.keyCode == 27){ //Esc
+    }else if(pressed.keyCode === 27){ //Esc
       $('#edit_panel').hide();
-    }else if(pressed.keyCode == 8 && pressed.shiftKey){ //shift+backspace
+    }else if(pressed.keyCode === 8 && pressed.shiftKey){ //shift+backspace
       $('#edit_field_html').val("");
       clickedElement = updateHTML("", clickedElement)[0];
     }
-  })
+  });
 
 });
